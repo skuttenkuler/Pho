39681e860991e5b0ab8d4a09bac3f22f8d97eb0a
skuttenkuler <samkiddmusic@gmail.com>
2020-01-21 23:00:03 -0800

    server.js complete

---
+++ server.js
@@ -0,0 +1,24 @@
+//express
+const express = require("express");
+//handlebars
+const ehb = require("express-handlebars")
+
+//port
+const PORT = process.env.PORT || 8080
+//app express
+const app = express()
+//use handlebars
+app.engine("handlebars", ehb({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//use static
+app.use(express.static("public"));
+
+//routes
+const routes = require("./controllers/burgers_controller.js")
+app.use(routes);
+
+//start Server
+app.listen(PORT, function(){
+    console.group("Server is listening on port " + PORT)
+});
